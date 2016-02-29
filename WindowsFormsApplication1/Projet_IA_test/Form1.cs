2ad09a6f5c5feb50a880177e4ae570d016276e6f
gandhi33 <alexis.deny@gmail.com>
2016-02-29 17:33:32 +0100

    Introduction de la classe Node pour les questions 2 à 4
    A modifier : le endstate pour que ça colle avec la question 2

--- WindowsFormsApplication1/Projet_IA_test/Form1.cs
+++ WindowsFormsApplication1/Projet_IA_test/Form1.cs
@@ -76,5 +76,53 @@ namespace Projet_IA_test
 
             return _nbImpasse;
         }
+
+        /*public string plusCourtChemin(int[,] matrice, char a, char b)
+        {
+            int depart = lettreAColonne(a);
+            int arrivee = lettreAColonne(b);
+            string res = "" + a;
+            List<string> possibilites = tousLesChemins(matrice, a, b);
+            string plusCourt = compareChemins(matrice, possibilites);
+            return plusCourt;
+        }
+
+        public List<string> tousLesChemins(int[,] matrice, char a, char b)
+        {
+            int depart = lettreAColonne(a);
+            int arrivee = lettreAColonne(b);
+            string res = "" + a;
+            List<string> possibilites = new List<string>();
+            for (int i = 0; i < 23; i++)
+            {
+                if (matrice[depart, i] != 0 && i != lettreAColonne(b))
+                {
+                    List<string> c = tousLesChemins(matrice, colonneAlettre(i), b);
+                    foreach (string s in c)
+                    {
+                        possibilites.Add(res + s);
+                    }
+                }
+            }
+            return possibilites;
+        }
+
+        public string compareChemins(int[,] matrice, List<string> possibilites)
+        {
+            string res = possibilites[0];
+
+        }
+
+        public char colonneAlettre(int a)
+        {
+            char res = (char) (a + 65);
+            return res;
+        }
+
+        public int lettreAColonne(char a)
+        {
+            int res = a - 65;
+            return res;
+        }*/
     }
 }
